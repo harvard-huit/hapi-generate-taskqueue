ac33add5a797361712f4f4893adc99551426a514
Mark Stacy <mbstacy@gmail.com>
2020-04-29 11:09:38 -0600

    updated version and clean up unused

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='genWorkflowTaskQueue',
-      version='0.3',
-      packages= find_packages(),
+      version='0.4',
+      packages=find_packages(),
       install_requires=[
           'jinja2',
       ],
       scripts=['genWorkflowTaskQueue']
-      #entry_points={
-      #  'console_scripts': [
-      #      'genWorkflowTaskQueue = genWorkflowTaskQueue',
-      #  ]
-      #}
-)
+
+      )
